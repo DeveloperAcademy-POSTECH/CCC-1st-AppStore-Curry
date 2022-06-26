14320faa7a7901ce81b3e7f60d7617fd15d9fb26
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-06-26 17:47:15 +0900

    UIKit 코드개발 세팅

---
+++ AppStore/AppStore/TabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarController.swift
+//  AppStore
+//
+//  Created by 이재웅 on 2022/06/26.
+//
+
+import UIKit
+
+class TabBarController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
