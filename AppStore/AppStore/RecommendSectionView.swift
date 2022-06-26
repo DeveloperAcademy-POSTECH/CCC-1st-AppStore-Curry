8148d0619518b92153e715acc2e64a93d07abbb4
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-06-26 21:44:58 +0900

    feat-TabBarController: RecommendSectionView 생성, collectionView 추가

--- AppStore/AppStore/RecommendSectionView.swift
+++ AppStore/AppStore/RecommendSectionView.swift
@@ -5,4 +5,47 @@
 //  Created by 이재웅 on 2022/06/26.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class RecommendSectionView: UIView {
+    private lazy var collectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.isPagingEnabled = true
+        collectionView.backgroundColor = .systemBackground
+        collectionView.showsHorizontalScrollIndicator = false
+       
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension RecommendSectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+         return UICollectionViewCell()
+    }
+    
+}
+
+extension RecommendSectionView: UICollectionViewDelegateFlowLayout {
+    
+}
+
